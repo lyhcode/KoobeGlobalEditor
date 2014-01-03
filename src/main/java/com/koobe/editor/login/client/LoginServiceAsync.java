b5fc10d68b0a6162c10e86ed46bfd452116762e9
kyle <kyle@630e58c9-a243-ca41-a344-d6b958b6d309>
2014-01-03 08:27:37 +0000

    add KoobeCommonData service
    git-svn-id: https://svn.koobe.com.tw:8443/svn/KoobeSystem3/KoobeGlobalEditor@974 630e58c9-a243-ca41-a344-d6b958b6d309

--- src/main/java/com/koobe/editor/login/client/LoginServiceAsync.java
+++ src/main/java/com/koobe/editor/login/client/LoginServiceAsync.java
@@ -1,11 +1,12 @@
 package com.koobe.editor.login.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.koobe.editor.login.shared.LoginResult;
 
 /**
  * The async counterpart of <code>LoginService</code>.
  */
 public interface LoginServiceAsync {
-    void auth(String username, String password, AsyncCallback<String> callback)
+    void auth(String username, String password, AsyncCallback<LoginResult> callback)
             throws IllegalArgumentException;
 }
