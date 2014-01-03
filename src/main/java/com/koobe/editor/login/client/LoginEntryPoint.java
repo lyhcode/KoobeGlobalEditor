d30103598b7cd1f7fe284c05ff27f474899120d8
lyhcode <lyhcode@gmail.com>
2014-01-03 16:23:39 +0800

    add KoobeCommonData service

--- src/main/java/com/koobe/editor/login/client/LoginEntryPoint.java
+++ src/main/java/com/koobe/editor/login/client/LoginEntryPoint.java
@@ -7,15 +7,14 @@ import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.koobe.editor.login.shared.LoginResult;
+import org.gwtbootstrap3.client.ui.*;
 import org.gwtbootstrap3.extras.bootbox.client.Bootbox;
-import org.gwtbootstrap3.client.ui.Button;
-import org.gwtbootstrap3.client.ui.FormGroup;
-import org.gwtbootstrap3.client.ui.PasswordTextBox;
-import org.gwtbootstrap3.client.ui.TextBox;
 import org.gwtbootstrap3.client.ui.constants.ValidationState;
 
 import static com.google.gwt.query.client.GQuery.$;
@@ -36,13 +35,20 @@ public class LoginEntryPoint implements EntryPoint {
     private static Binder uiBinder = GWT.create(Binder.class);
 
     @UiField
+    Container container;
+
+    @UiField
     Button loginButton;
+
     @UiField
     TextBox usernameTextBox;
+
     @UiField
     FormGroup usernameFormGroup;
+
     @UiField
     PasswordTextBox passwordTextBox;
+
     @UiField
     FormGroup passwordFormGroup;
 
@@ -70,23 +76,34 @@ public class LoginEntryPoint implements EntryPoint {
         String username = usernameTextBox.getText();
         String password = passwordTextBox.getText();
 
-        loginService.auth(username, password, new AsyncCallback<String>() {
+        loginService.auth(username, password, new AsyncCallback<LoginResult>() {
             @Override
             public void onFailure(Throwable throwable) {
                 Bootbox.alert("Request failed.");
             }
 
             @Override
-            public void onSuccess(String result) {
-                if ("ok1".equals(result)) {
+            public void onSuccess(LoginResult result) {
+                if (result.isSuccess()) {
                     changeValidationState(ValidationState.SUCCESS);
 
-                    String queryString = Window.Location.getQueryString();
-                    Window.Location.assign("/" + queryString);
+                    new Timer() {
+                        @Override
+                        public void run() {
+                            String queryString = Window.Location.getQueryString();
+                            Window.Location.assign("/" + queryString);
+                        }
+                    }.schedule(1500);
+
+                    $("#page").as(UiEffects).effect("drop");
+
+
                 } else {
                     changeValidationState(ValidationState.ERROR);
 
                     $("#loginbox").as(UiEffects).effect("shake");
+
+                    Bootbox.alert(result.getMessage());
                 }
             }
         });
--- src/main/java/com/koobe/editor/login/client/LoginService.java
+++ src/main/java/com/koobe/editor/login/client/LoginService.java
@@ -2,11 +2,12 @@ package com.koobe.editor.login.client;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.koobe.editor.login.shared.LoginResult;
 
 /**
  * The client side stub for the RPC service.
  */
 @RemoteServiceRelativePath("login")
 public interface LoginService extends RemoteService {
-    String auth(String username, String password) throws IllegalArgumentException;
+    LoginResult auth(String username, String password) throws IllegalArgumentException;
 }
