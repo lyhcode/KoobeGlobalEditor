42c3f361bdf6236afa21f1980b1187cf16193824
cloude <cloude@630e58c9-a243-ca41-a344-d6b958b6d309>
2014-01-23 06:48:38 +0000

    Add codes for demo purpose
    git-svn-id: https://svn.koobe.com.tw:8443/svn/KoobeSystem3/KoobeGlobalEditor@1384 630e58c9-a243-ca41-a344-d6b958b6d309

--- src/main/java/com/koobe/editor/admin/client/application/monitor/MonitorView.java
+++ src/main/java/com/koobe/editor/admin/client/application/monitor/MonitorView.java
@@ -16,16 +16,24 @@
 
 package com.koobe.editor.admin.client.application.monitor;
 
+import static com.google.gwt.query.client.GQuery.$;
+
+import org.gwtbootstrap3.client.ui.Button;
+
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
-import org.gwtbootstrap3.client.ui.Button;
-
-import static com.google.gwt.query.client.GQuery.$;
+import com.koobe.common.gwt.demo.service.client.MyDemoService;
+import com.koobe.common.gwt.demo.service.client.MyDemoServiceAsync;
+import com.koobe.common.model.enums.FileTypeEnum;
+import com.koobe.common.model.message.ConvertEpubRequest;
+import com.koobe.common.model.message.ExtractEpubRequest;
 
 public class MonitorView extends ViewImpl implements MonitorPresenter.MyView {
     interface Binder extends UiBinder<Widget, MonitorView> {
@@ -38,11 +46,45 @@ public class MonitorView extends ViewImpl implements MonitorPresenter.MyView {
 
     @UiField
     Button refreshButton;
+    
+    MyDemoServiceAsync service = GWT.create(MyDemoService.class);
 
     @UiHandler("refreshButton")
     void onRefreshButtonClick(ClickEvent event) {
         //Window.alert("rest");
 
-        $("#monitor-table-body").append("<tr><td>123</td></tr>");
+//        $("#monitor-table-body").append("<tr><td>123</td></tr>");
+    	
+//    	Book book = new Book();
+//    	book.setBucket("aaaa");
+        
+        service.getExtractEpubRequest(new AsyncCallback<ExtractEpubRequest>() {
+			@Override
+			public void onSuccess(ExtractEpubRequest result) {
+				String value = result.getAction() + result.getDestBucket() + result.getDestPath() + 
+						result.getSrcBucket() + result.getSrcPath();
+				$("#monitor-table-body").append("<tr><td>"+value+"</td></tr>");
+			}
+			
+			@Override
+			public void onFailure(Throwable caught) {
+				$("#monitor-table-body").append("<tr><td>"+caught.getMessage()+"</td></tr>");
+			}
+		});
+        
+        service.getConvertEpubRequest(new AsyncCallback<ConvertEpubRequest>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				$("#monitor-table-body").append("<tr><td>"+caught.getMessage()+"</td></tr>");
+			}
+
+			@Override
+			public void onSuccess(ConvertEpubRequest result) {
+				String value = result.getDestFileType().toString() + result.getAction() + result.getSrcPath();
+				FileTypeEnum t = result.getSrcFileType();
+				$("#monitor-table-body").append("<tr><td>"+value+"</td></tr>" + "<tr><td>"+t.toString()+"</td></tr>");
+			}
+		});
     }
 }
