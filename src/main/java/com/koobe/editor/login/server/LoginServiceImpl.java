b592631b330999b51ba424141e248cde3bb0f97a
cloude <cloude@630e58c9-a243-ca41-a344-d6b958b6d309>
2014-01-24 09:58:46 +0000

    Add autowired user repository
    git-svn-id: https://svn.koobe.com.tw:8443/svn/KoobeSystem3/KoobeGlobalEditor@1393 630e58c9-a243-ca41-a344-d6b958b6d309

--- src/main/java/com/koobe/editor/login/server/LoginServiceImpl.java
+++ src/main/java/com/koobe/editor/login/server/LoginServiceImpl.java
@@ -1,31 +1,43 @@
 package com.koobe.editor.login.server;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import com.koobe.common.core.KoobeApplication;
-import com.koobe.common.data.KoobeDataService;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.koobe.common.data.domain.User;
 import com.koobe.common.data.repository.UserRepository;
+import com.koobe.common.gwt.server.servlet.GenericRemoteServiceServlet;
 import com.koobe.editor.login.client.LoginService;
 import com.koobe.editor.login.shared.LoginResult;
-
-import java.util.List;
 
 /**
  * The server side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
-public class LoginServiceImpl extends RemoteServiceServlet implements
-        LoginService {
+@Controller
+@RequestMapping("/login.rpc")
+public class LoginServiceImpl extends GenericRemoteServiceServlet implements LoginService {
 
-    KoobeApplication application = KoobeApplication.getInstance();
-    KoobeDataService dataService = (KoobeDataService)application.getService(KoobeDataService.class);
-    UserRepository userRepository = (UserRepository)dataService.getRepository(UserRepository.class);
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+    UserRepository userRepository;
+	
+//	@PersistenceContext
+//	EntityManager entityManager;
 
     public LoginResult auth(String username, String password) throws IllegalArgumentException {
-
+    	
+    	// userRepository = (UserRepository) koobeDataService.getRepository(UserRepository.class);
+    	
         List<User> users = userRepository.findByUserId(username);
 
         if (users.size() == 0) {
+        	log.info("error account or password");
             return new LoginResult(false, "帳號或密碼錯誤。");
         }
 
